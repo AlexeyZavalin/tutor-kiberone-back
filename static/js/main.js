1cba73b736aaa9a1bced6b9c6c9285b467bfda62
alexey <zavalin@infodev.ru>
2021-08-22 13:12:31 +1000

    handling group remove form

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let removeFormGroupId = document.getElementById('id_group_id');
 
 let btnsModal = document.querySelectorAll('.btn_modal');
@@ -29,5 +31,48 @@ window.onclick = function (event) {
             element.style.display = "none";
             removeFormGroupId.value = null;
         }
+    })
+}
+
+async function postData(url = '', data = {}) {
+    const csrftoken = getCookie('csrftoken')
+    const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken
+        },
+        body: JSON.stringify(data)
+    });
+    return await response.json();
+}
+
+// удаление группы
+let removeGroupForm = document.getElementById('removeGroupForm');
+
+if (removeGroupForm) {
+    removeGroupForm.addEventListener('submit', function (e) {
+        e.preventDefault()
+        if (removeGroupForm.querySelector('.form_errors')) {
+            removeGroupForm.querySelector('.form_errors').remove()
+        }
+        const url = removeGroupForm.dataset['action']
+        const groupId = removeGroupForm.querySelector('#id_group_id').value
+        const password = removeGroupForm.querySelector('#id_password').value
+        postData(url, {'group_id': groupId, 'password': password})
+            .then((data) => {
+                if (data['success']) {
+                    document.querySelector('#group-row-' + groupId).remove();
+                } else {
+                    let errorSpan = document.createElement('div')
+                    errorSpan.textContent = data['message']
+                    errorSpan.classList.add('form_errors')
+                    const btn = removeGroupForm.querySelector('.btn')
+                    const parent = btn.parentNode
+                    parent.insertBefore(errorSpan, btn)
+                }
+            });
     })
 }
