bac1939d0e6ff5e9c8ca66184595f1585eedcd0a
Alexey Zavalin <lehazavalin95@gmail.com>
2021-11-29 00:09:32 +1000

    добавил модели для ярмарки

---
+++ fair/models.py
@@ -0,0 +1,83 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils import timezone
+
+from mainapp.mixins import DeletedMixin
+
+
+class Souvenir(DeletedMixin):
+    """ Модель сувенира ярмарки """
+    name = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        verbose_name='Имя сувенира')
+    image = models.ImageField(
+        null=True,
+        blank=True,
+        verbose_name='Фото сувенира',
+        upload_to='fair_souvenirs'
+    )
+    price = models.DecimalField(
+        validators=[MinValueValidator(0), MaxValueValidator(3000)],
+        verbose_name='Стоимость в киберонах',
+        null=False,
+        blank=False,
+        max_digits=4,
+        decimal_places=0
+    )
+
+    class Meta:
+        verbose_name = 'Сувенир'
+        verbose_name_plural = 'Сувениры'
+        ordering = ('price',)
+
+    def __str__(self):
+        return f'{self.name} - {self.price}K'
+
+
+class FairRegistration(models.Model):
+    """ Модель записи с сувенирами для студента """
+    student = models.ForeignKey(
+        'mainapp.Student',
+        verbose_name='Ученик',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='students'
+    )
+    date = models.DateField(
+        auto_now_add=True,
+        verbose_name='Дата'
+    )
+
+    class Meta:
+        verbose_name = 'Запись о ярмарке для ученика'
+        verbose_name_plural = 'Записи о ярмарке для учеников'
+
+    def __str__(self):
+        return f'{self.student.name} - {self.date}'
+
+
+class FairRegistrationSouvenir(models.Model):
+    """ Сувенир в записи ярмарки для ученика """
+    souvenir = models.ForeignKey(
+        Souvenir,
+        verbose_name='Сувенир',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    fair_registration = models.ForeignKey(
+        FairRegistration,
+        on_delete=models.CASCADE,
+        verbose_name='Запесь о ярмарке',
+        related_name='souvenirs'
+    )
+
+    class Meta:
+        verbose_name = 'Сувенир в записи о ярмарке'
+        verbose_name_plural = 'Сувениры в запясях о ярмарках'
+
+    def __str__(self):
+        return self.souvenir.__str__()
