b6bd03846202ffa9cf1d3e233f91694faa8367a2
Alexey Zavalin <lehazavalin95@gmail.com>
2021-12-14 01:59:53 +1000

    удалил лишний файл

--- fair/static/js/main.js
+++
@@ -1,9 +0,0 @@
-const fairBtns = document.querySelectorAll('.btn_fair')
-
-if (fairBtns.length) {
-    fairBtns.forEach(function(element) {
-        element.addEventListener('click', function (event) {
-            const studentId = element.data['studnetId'];
-        })
-    })
-}
