5d36a8471d3c784f047fb95c34d2a3fb1e56e234
Alexey Zavalin <lehazavalin95@gmail.com>
2021-12-14 01:58:39 +1000

    доработана админка для ярмарки

--- fair/admin.py
+++ fair/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
-from fair.models import FairRegistration, Souvenir
+from django.db.models import Sum
+
+from fair.models import FairRegistration, FairRegistrationSouvenir, Souvenir
+
+
+class SouvenirInline(admin.StackedInline):
+    model = FairRegistrationSouvenir
+    readonly_fields = ['price']
+    exclude = ['souvenir']
+    extra = 0
+    can_delete = False
+    verbose_name = 'Сувенир'
+    verbose_name_plural = 'Сувениры'
 
 
 @admin.register(Souvenir)
 class SouvenirAdmin(admin.ModelAdmin):
     """ Админка сувенира """
-    list_display = ['name', 'is_deleted']
+    list_display = ['name', 'price', 'is_deleted']
+    extra = 0
 
 
 @admin.register(FairRegistration)
@@ -14,3 +27,10 @@ class FairRegistration(admin.ModelAdmin):
     date_hierarchy = 'date'
     list_filter = ['student__group__tutor', 'student__group', 'date']
     search_fields = ['student__name', 'name']
+    list_display = ['student', 'date']
+    readonly_fields = ['student', 'date', 'total']
+    inlines = [SouvenirInline]
+
+    def total(self, object):
+        """ получаем сумму в киберонах """
+        return object.souvenirs.all().aggregate(Sum('price')).get('price__sum')
