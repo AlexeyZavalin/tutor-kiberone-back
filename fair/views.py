e5ea13a5c7d9787b45934cb62dc33cdaf792c014
Alexey Zavalin <lehazavalin95@gmail.com>
2021-12-26 16:55:17 +1000

    изменил представление: фильтр по количеству, права доступа к списку сувениров, обработку исключения, если ученик не найден

--- fair/views.py
+++ fair/views.py
@@ -1,38 +1,45 @@
 import json
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse
 
 from django.template.loader import render_to_string
 from django.views import View
-from django.views.generic import CreateView
 
 from fair.models import Souvenir, FairRegistration
 from fair.services import get_response_for_create_fair
 from mainapp.models import Student
 
 
+@login_required
 def souvenirs_list(request):
     """
         список киберприятностей
     """
-    objects = Souvenir.objects.filter(is_deleted=False)
+    qs = Souvenir.objects.filter(is_deleted=False, amount__gt=0)
     if 'id' in request.GET:
-        student = Student.objects.get(id=request.GET['id'])
-        objects = objects.filter(price__lte=student.kiberon_amount)
+        try:
+            student = Student.objects.get(id=request.GET['id'])
+            qs = qs.filter(price__lte=student.kiberon_amount)
+        except ObjectDoesNotExist:
+            # ученик не найден
+            pass
     template = render_to_string(
-        'fair/souvenir/list.html', {'souvenirs': objects}
+        'fair/souvenir/list.html', {'souvenirs': qs}
     )
     return JsonResponse({'markup': template})
 
 
-class FairRegistrationCreateView(View):
+class FairRegistrationCreateView(View, LoginRequiredMixin):
     """
     Представление для создания записи о проведенной ярмарке для ученика
     """
     model = FairRegistration
 
-    def post(self, request, *args, **kwargs):
-        body = json.loads(request.body)
+    def post(self, *args, **kwargs):
+        body = json.loads(self.request.body)
         items = body['items']
         student_id = body['studentId']
         balance = body['balance']
