b849bd39afe09cdb8a184d4e96226d73cacc0113
Alexey Zavalin <lehazavalin95@gmail.com>
2022-11-24 20:48:11 +1000

    исправил настройки

--- kiberone_tutor/base.py
+++ kiberone_tutor/base.py
@@ -78,6 +78,17 @@ TEMPLATES = [
     },
 ]
 
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('SQL_DATABASE', BASE_DIR / 'db.sqlite3'),
+        'USER': os.environ.get('SQL_USER', 'user'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
+        'HOST': os.environ.get('SQL_HOST', 'localhost'),
+        'PORT': os.environ.get('SQL_PORT', '5432'),
+    }
+}
+
 WSGI_APPLICATION = 'kiberone_tutor.wsgi.application'
 
 
