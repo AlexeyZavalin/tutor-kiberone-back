8b4482b65caacd3eabf8a8daca70edd68f5d9725
Alexey Zavalin <lehazavalin95@gmail.com>
2022-12-10 20:09:21 +1000

    изменения логики получния товаров для ярмарки (теперь не выводятся с остатками 0)

--- fair/views.py
+++ fair/views.py
@@ -1,10 +1,9 @@
 import json
 
 from django.http import JsonResponse
+from django.core import serializers
 
-from django.template.loader import render_to_string
 from django.views import View
-from django.views.generic import CreateView
 
 from fair.models import Souvenir, FairRegistration
 from fair.services import get_response_for_create_fair
@@ -15,14 +14,14 @@ def souvenirs_list(request):
     """
         список киберприятностей
     """
-    objects = Souvenir.objects.filter(is_deleted=False)
+    objects = Souvenir.objects.filter(is_deleted=False, amount__gt=0)
     if 'id' in request.GET:
         student = Student.objects.get(id=request.GET['id'])
-        objects = objects.filter(price__lte=student.kiberon_amount)
-    template = render_to_string(
-        'fair/souvenir/list.html', {'souvenirs': objects}
-    )
-    return JsonResponse({'markup': template})
+        objects = objects.filter(
+            price__lte=student.kiberon_amount,
+        )
+
+    return JsonResponse(serializers.serialize('json', objects), safe=False)
 
 
 class FairRegistrationCreateView(View):
